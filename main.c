f10cc2120f22a8c3c55d6e9a0ce157be30e4f096
Selim Efe <uysalselimefe@gmail.com>
2026-01-17 22:14:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,40 +4,45 @@
 
 int main()
 {
-        int i,n,sayi,sum,ort;
+    int i, n, sayi, sum, ort;
 
-    sum=0;
+    sum = 0;
 
     printf("n i girin ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     int array[n];
 
     srand(time(NULL));
 
-    for(i=1;i<=n;i++){
-        sayi=rand()%101;
-        printf("%d \n",sayi);
+    for (i = 1; i <= n; i++)
+    {
+        sayi = rand() % 101;
+        printf("%d \n", sayi);
 
-        array[i-1]=sayi;
-        sum+=sayi;
+        array[i - 1] = sayi;
+        sum += sayi;
     }
-    ort=sum/n;
-    printf("ortalama: %d",ort);
+    ort = sum / n;
+    printf("ortalama: %d", ort);
 
-    for(i=0;i<=n-1;i++){
-        if(array[i]<ort){
-            do{
-                sayi=rand()%101;
-            }while(sayi<=ort);
+    for (i = 0; i <= n - 1; i++)
+    {
+        if (array[i] < ort)
+        {
+            do
+            {
+                sayi = rand() % 101;
+            } while (sayi <= ort);
 
-            array[i]=sayi;
+            array[i] = sayi;
         }
     }
 
     printf("\n");
 
-    for(i=0;i<=n-1;i++){
-        printf("%d \n",array[i]);
+    for (i = 0; i <= n - 1; i++)
+    {
+        printf("%d \n", array[i]);
     }
 }
